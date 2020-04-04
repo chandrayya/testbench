502c52c69bb8fbb35c91ed4e9f6a2bc9961c640e
chandrayya <tpbd@gmail.com>
2020-04-04 16:25:38 +0530

    Day 1 work

--- com.chandrayya.testbench/src/com/chandrayya/testbench/HelloWorld.java
+++ com.chandrayya.testbench/src/com/chandrayya/testbench/HelloWorld.java
@@ -1,12 +1,59 @@
 package com.chandrayya.testbench;
 
-public class HelloWorld {
+/**
+ * 
+ * @author chandrayya
+ *
+ *
+ */
+public class HelloWorld {	
 	
+	/**
+	 * 
+	 * @param args This is an array of command line arguments.
+	 */
 	public static void main(String[] args) {
-		//Printing hello world ten times.
-		int i = 10;
-		for (int j = 0; j < i; j++) {			
-			System.out.println("Hello world");
-		}
+				
+		int a = 10;
+		int b = 20;
+		
+		//doCalculateAverage(a,b);		
+		doGenerateFibbonacciNumbers(b);
 	}
+
+	private static void doGenerateFibbonacciNumbers(int a) {
+		// 0,1,1,2,3,5....
+		// 0,1,1,2,3,4,5,6,7,8,9,
+		int firstNumber = 0;
+		int secondNumber = 1;
+		int nextNumber;
+		for (int i = 0; i < a; i++) {		
+			if (i == 0) {
+				firstNumber = i;
+				System.out.print(i + ",");
+				continue;
+			}		
+			if (i == 1) {
+				secondNumber = i;
+				System.out.print(i + ",");
+				continue;
+			}		
+			
+			nextNumber = firstNumber + secondNumber;
+			
+			System.out.print(nextNumber + ",");
+			
+			firstNumber = secondNumber;
+			secondNumber = nextNumber;
+		}
+
+	}
+
+	private static void doCalculateAverage(int a, int b) {
+		
+		double average = (a + b) / 2;		
+		System.out.println("Average of a and b " + average);
+		
+	}	
+	
 }
