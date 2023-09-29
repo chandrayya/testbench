5242a46e5dc8d2865c862abbbdd48a8adc5c49a0
Chandrayya G K <chandrayya.gk@gmail.com>
2023-09-29 10:34:50 +0530

    Initialization removed

--- com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
+++ com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
@@ -1,13 +1,10 @@
 package com.chandrayya.testbench.assignments;
 
 public class PrintPatterns {
-public static int ROW_COUNT = 5;
-	public static int COL_COUNT = 5;
 	
-	public static void main(String[] args) {
-		
-		int rows = ROW_COUNT;
-		int cols = COL_COUNT;
+	public static void main(String[] args) {		
+		int rows = 0;
+		int cols = 0;
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < cols; j++) {
 				double midEle = Math.ceil(rows / 2);
