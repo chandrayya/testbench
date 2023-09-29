865779573aa386dc1b5f647feee1ebcd12411e3f
Chandrayya G K <chandrayya.gk@gmail.com>
2023-09-29 08:22:48 +0530

    constants are added

--- com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
+++ com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
@@ -1,11 +1,13 @@
 package com.chandrayya.testbench.assignments;
 
 public class PrintPatterns {
-
+public static int ROW_COUNT = 5;
+	public static int COL_COUNT = 5;
+	
 	public static void main(String[] args) {
 		
-		int rows = 5;
-		int cols = 5;
+		int rows = ROW_COUNT;
+		int cols = COL_COUNT;
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < cols; j++) {
 				double midEle = Math.ceil(rows / 2);
