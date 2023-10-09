192353ce67f56b27f2666e84b8439821ba8b259b
Chandrayya <chandrayya@knstek.com>
2023-10-09 14:52:36 +0530

    Revert "Commit message check"
    This reverts commit 860b9850b2aafb0b270b380ba3b30b83ed52d15e.

--- com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
+++ com.chandrayya.testbench/src/com/chandrayya/testbench/assignments/PrintPatterns.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *  Prints patterns on console
- */
 public class PrintPatterns {
 	
 	public static void main(String[] args) {
